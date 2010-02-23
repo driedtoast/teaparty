40e03d7f39d1e91affbc2772d33f5a6b7fc79f48
Daniel Marchant <driedtoast@toaster.local>
2010-02-23 10:22:00 -0500

    adding initial startup scripts and structure

--- teaparty/settings.py
+++ teaparty/settings.py
@@ -4,17 +4,17 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    ('@ADMIN_NAME@', '@ADMIN_EMAIL@'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/opt/teaparty/sqlite.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = '@DATABASE_ENGINE@'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = '@DATABASE_NAME@'             # Or path to database file if using sqlite3.
+DATABASE_USER = '@DATABASE_USER@'             # Not used with sqlite3.
+DATABASE_PASSWORD = '@DATABASE_PASSWORD@'         # Not used with sqlite3.
+DATABASE_HOST = '@DATABASE_HOST@'             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = '@DATABASE_PORT@'             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -35,12 +35,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '@MEDIA_ROOT@'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '@MEDIA_URL@'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -48,7 +48,7 @@ MEDIA_URL = ''
 ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'm^uayo1oqf^8z2*%-^v6owqi3zol=#5^$t*y(su7+#o)vbmpz4'
+SECRET_KEY = '@SECRET_KEY@'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -66,6 +66,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'teaparty.urls'
 
 TEMPLATE_DIRS = (
+    '@TEMPLATE_DIR@'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
