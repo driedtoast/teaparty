ec3983c2977ea21bbf986d95f263dec02be8f3a3
Daniel Marchant <driedtoast@toaster.local>
2010-02-25 10:16:13 -0500

    adding models

--- teaparty/main/models.py
+++ teaparty/main/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Instance(models.Model):
-	id = models.CharField(max_length=100)
+	instanceid = models.CharField(max_length=100)
         type = models.CharField(max_length=100)
 	def __unicode__(self):
         	return self.id
 
 class ServerImage(models.Model):
-	id = models.CharField(max_length=100)
+	imageid = models.CharField(max_length=100)
 	name = models.CharField(max_length=200)
 	created = models.DateTimeField('date created')
 	type = models.CharField(max_length=100)
