386fb01f2f04aa00c20fa091e0606134e660bcee
Dan Marchant <dmarchant@difficulty.(none)>
2010-07-12 14:56:43 -0400

    adding some initial sqlalchemy

--- apps/teaparty/tp/services/__init__.py
+++ apps/teaparty/tp/services/__init__.py
@@ -4,14 +4,17 @@ import tp.models as models
 
 ### Class to manage the ami images
 class ImageService(object):
-    def __init__():
+    def __init__(self):
         db.initdb()
     
-    def save(amazonImage):
+    def save(self,amazonImage):
         session = db.session()
         session.add(amazonImage);
         session.flush()
         
-    def get(amiid):
+    def get(self,amiid):
         session = db.session()
-        return session.query(models.AmazonImage).filter(AmazonImage.id==amiid).one()
+        try:
+            return session.query(models.AmazonImage).filter(models.AmazonImage.amazon_id==amiid).one()
+        except Exception, e:
+            return None
